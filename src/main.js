3cbf238bba7a052f21a32b7c086eb0d4863e47a9
SinaisAna <58980145+SinaisAna@users.noreply.github.com>
2020-08-13 09:44:07 -0500

    avance de nuestro html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
 
-console.log(example, data);
+const bienvenido =()=>{
+	document.querySelector('#conter').style.display="none";
+	document.querySelector('#contenido').style.display="block";
+	console.log(document.querySelector('#contenido'))
+}
+
+document.querySelector('#primerBoton').addEventListener('click',bienvenido);
+
+const paginaPrin =()=>{
+	document.querySelector('#contenido').style.display="block";
+}
+
+// import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
+//import data from './data/rickandmorty/rickandmorty.js';
