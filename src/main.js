f9e8db148a7171068d224814bd5036cba9a4bb08
SinaisAna <58980145+SinaisAna@users.noreply.github.com>
2020-08-15 22:46:28 -0500

    avances de la tada primer HU

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
+//import datajs  from './data.js';
+const data = window.rickAndMorty.results;
+console.log(data);
 
+const bienvenido =()=>{
+	document.querySelector('#conter').classList.add("ocultar");
+	document.querySelector('#contenido').classList.remove("ocultar");
+	document.querySelector('#container-header').classList.remove("ocultar");
+	function obtenerPersonajes (data) {
+		return `<div class="person">
+		<div class="imagenes">
+		<img class="photo" src="${data.image}">
+		</div>
+		<div class="info">
+		<h2 class="name">${data.name}</h2>
+		<p class="origin">Genero:${data.gender}</p>
+		<p class="origin">Origen:${data.origin.name}</p>
+		<p class="origin">Especie:${data.species}</p>
+		<p class="origin">Estado:${data.status}</p>
+		</div>
+		</div>`
+	}
+	document.getElementById("all-data").innerHTML =`
+	<h1 class="app-title">Total de Personajes(${data.length})</h1>
+	${data.map(obtenerPersonajes).join(" ")}
+	`
+	console.log("all-data");
+}
 //Botón de inicio
 const btnIntro = document.getElementById("btnIntro")
 const btnIngresar = document.createElement("button");
@@ -58,20 +85,6 @@ const btnEstado = document.getElementById("btn-estado")
 btnEstado.addEventListener("click", checkboxEs);
 
 
-
-const checki =()=>{
-document.querySelector('#check').classList.remove('ocultar');
-
-const paginaPrin = () => {
-	document.querySelector('#contenido').style.display = "block";
-}
-
-document.querySelector('#especies').addEventListener('click',checki);
-
-const checkbox =()=>{
-	
-}
- 
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 //import data from './data/rickandmorty/rickandmorty.js';
