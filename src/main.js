7b855e9fb54d6be5aa5b2bc24a0b86866a49eb5c
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-08-18 11:07:31 -0500

    funcion filter tipo con bug

--- src/main.js
+++ src/main.js
@@ -3,33 +3,47 @@ import { example } from './data.js'; //fijo
 import data from './data/pokemon/pokemon.js';  //fijo
 // import data from './data/rickandmorty/rickandmorty.js';
 
-let allPokemon = data.pokemon;
-
-function pokemonTemplate(poke){
-    return `
-    <div class= 'poke'>
-    <img class= 'poke-img'  src= '${poke.img}'>
-    <h2 class= 'poke-name'>${poke.name}</h2>
-    </div>
-    `
-}
-
-document.getElementById('root').innerHTML = `
-<h2 class = 'rootTitle'>Pokemons (${allPokemon.length}results)</h2>
-${allPokemon.map(pokemonTemplate).join('')}
+let allPokemon= data.pokemon;
+const section = document.querySelector('section');
+section.innerHTML=`
+<select id='list'> 
+<option value=''>All</option>
+<option value='grass'>Hierba</option>
+<option value='fire'>Fuego</option>
+<option value='fairy'>Hada</option>
+<option value='electric'>Electrico</option>
+<option value='rock'>Roca</option>
+<option value='water'>Agua</option>
+</select>
 `
-   /* return `
-    <div class='pokemon'>
-      <img class='pok-img' src='${pokemon.img}'>
-      <h2 class='pok-name'>${pokemon.name}</h2>
-    </div>  
-    `*/
-
-
-
-
-
-
+let list= document.getElementById('list');
+list.addEventListener('change',(event)=> {
+    let selectValue = list.value;
+    console.log(selectValue);
+    
+        function pokemonFilter(poke){
+        return poke.type.includes(selectValue) 
+    }
+    
+    allPokemon = allPokemon.filter(pokemonFilter);
+    console.log(allPokemon);
+           
+    //mostrar pokémons
+    
+    function pokemonTemplate(poke){
+        return`
+        <div class='poke'>
+        <img class='poke-img' src='${poke.img}'>
+        <p class= 'poke-name'> ${poke.name}<p>    
+        </div>
+        `
+    }
+    
+    document.getElementById('root').innerHTML = `
+    <h2 class= 'rootTitle'>Pokemons (${allPokemon.length}results) <h2>
+    ${allPokemon.map(pokemonTemplate).join('')}
+    `
+})
 
 console.log(example, data);
 
