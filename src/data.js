dea2bcc693ef2c0aaddc3de93cb90cd9d4aad1e1
Elizabeth Rivera <elizabethriver@gmail.com>
2020-08-18 10:24:47 -0500

    construcción de la primera vista Historia de usuario

--- src/data.js
+++ src/data.js
@@ -7,3 +7,38 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//función para el top cinco//
+const getTopFive = (data) => {
+  const newArray = data.map(copyData => Object.assign({}, { name: copyData.name, hp: copyData.stats.hp, img: copyData.img }));
+  let newArrayOrder = newArray.sort((higher, lessHigher) => {
+    if (higher.hp <= lessHigher.hp) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return newArrayOrder.slice(0, 5);
+};
+
+
+function containerFunctions(array) {
+  // const array = Object.values(LOL.data);
+  // Segundo diseño de templates
+  const arrayTopFive = lol.getTopFive(array);
+  // templatepara el top ten
+  const templateListChampionTop = (list) => {
+    let championsList = [];
+    list.forEach((array) => {
+      const templateList =
+        `<div class='card-1'>
+      <img class='champion' src='lol.js/${array.name}.png'></img>
+      <h2 class='namechampion'>"${array.name}"</h2>
+      <h3 class='hptext'><img class='img-hp' src='imagenes/puntovida.PNG'>PUNTOS DE VIDA : ${array.hp}</h3>
+      </div>`;
+      championsList += templateList;
+    });
+    document.getElementById('list-top-champions').innerHTML = championsList;
+  };
+  templateListChampionTop(arrayTopFive);
+}
