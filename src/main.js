3a991cb401d7c6228adec8f846b158ad4d0c087f
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-08-18 10:23:01 -0500

    section en html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { example } from './data.js';
+import { example } from './data.js'; //fijo
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js'; //fijo
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+
+console.log(example, data); //fijo
