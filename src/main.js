077462d80724e6f896a878410478e1ffd94dd86a
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-08-18 22:34:46 -0500

    header color y viewport

--- src/main.js
+++ src/main.js
@@ -5,50 +5,55 @@ import data from './data/pokemon/pokemon.js';  //fijo
 
 let allPokemon= data.pokemon;
 
-function pokemonTemplate(poke){
-    return`
-    <div class='poke'>
-    <img class='poke-img' src='${poke.img}'>
-    <p class= 'poke-name'> ${poke.name}<p>    
-    </div>
+//mostrar pokémons
+    
+   function pokemonTemplate(poke){
+        return`
+        <div class='poke'>
+        <img class='poke-img' src='${poke.img}'>
+        <p class= 'poke-name'> ${poke.name}<p>    
+        </div>
+        `
+    }
+    
+    let root = document.getElementById('root');
+    root.innerHTML = `
+    <h2 class= 'rootTitle'>Pokemons (${allPokemon.length}results) <h2>
+    ${allPokemon.map(pokemonTemplate).join('')}
     `
-}
+   
+ // Seleccionar por tipo
 
-document.getElementById('root').innerHTML = `
-<h2 class= 'rootTitle'>Pokemons (${allPokemon.length}results) <h2>
-${allPokemon.map(pokemonTemplate).join('')}
-`
-
-
-
-const section = document.querySelector('section');
-section.innerHTML=`
-<select id='list'> 
-<option value=''>All</option>
-<option value='grass'>Hierba</option>
-<option value='fire'>Fuego</option>
-<option value='fairy'>Hada</option>
-<option value='electric'>Electrico</option>
-<option value='rock'>Roca</option>
-<option value='water'>Agua</option>
-</select>
-`
 let list= document.getElementById('list');
 list.addEventListener('change',(event)=> {
     let selectValue = list.value;
     console.log(selectValue);
-    
+ 
+  // Para ver si arreglo incluye valor seleccionado
         function pokemonFilter(poke){
         return poke.type.includes(selectValue) 
     }
-    allPokemon = allPokemon.filter(pokemonFilter);
-    console.log(allPokemon);
-           
-    //mostrar pokémons
+   // Filtra los pokemones 
+
+    let typePokemon = allPokemon.filter(pokemonFilter);
+    console.log(typePokemon);
+
+   // Crea estructura para pokemon filtrado      
+      function pokemonTemplateFilter(type){
+        return`
+        <div class='poke'>
+        <img class='poke-img' src='${type.img}'>
+        <p class= 'poke-name'> ${type.name}<p>    
+        </div>
+        `
+    }
+   // Mostrar Pokemones filtrados
+    root.innerHTML = `
+    <h2 class= 'rootTitle'>Pokemons (${typePokemon.length}results) <h2>
+    ${typePokemon.map(pokemonTemplateFilter).join('')}
+    `
     
-    
+
 })
 
 console.log(example, data);
-
-
