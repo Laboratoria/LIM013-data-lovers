112365ad45ce2ef44ad85c493b564828aebcbd80
MayraPerez97 <68233416+MayraPerez97@users.noreply.github.com>
2020-08-19 15:56:56 -0500

    Revert "filter characters and filter character by species with API url"

--- src/main.js
+++ src/main.js
@@ -1,33 +1,6 @@
-
-import { clickButtonNext, clickButtonPrevious, clickHuman, clickAlien, clickHumanoid, clickAnimal, clickMytholog, clickRobot, clickUnknown, clickCronenberg, clickPoopybutthole, clickDisease, clickPlanet } from './data.js';
+import { example } from './data.js';
+// import data from './data/lol/lol.js';
+import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-document.querySelector('#human').addEventListener('click', clickHuman);
-
-document.querySelector('#alien').addEventListener('click', clickAlien);
-
-document.querySelector('#humanoid').addEventListener('click', clickHumanoid);
-
-document.querySelector('#animal').addEventListener('click', clickAnimal);
-
-document.querySelector('#mytholog').addEventListener('click', clickMytholog);
-
-document.querySelector('#robot').addEventListener('click', clickRobot);
-
-document.querySelector('#unknown').addEventListener('click', clickUnknown);
-
-document.querySelector('#cronenberg').addEventListener('click', clickCronenberg);
-
-document.querySelector('#poopybutthole').addEventListener('click', clickPoopybutthole);
-
-document.querySelector('#disease').addEventListener('click', clickDisease);
-
-document.querySelector('#planet').addEventListener('click', clickPlanet);
-
-document.querySelector('#next').addEventListener('click', clickButtonNext);
-
-document.querySelector('#previous').addEventListener('click', clickButtonPrevious);
-
-
-
-
+console.log(example, data);
