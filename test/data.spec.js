9078e12411bc04bb7ee177673e540dcfda349b23
Sharon Huaman <sharonb.huaman@gmail.com>
2020-08-19 18:46:03 -0500

    cambiando order a objeto con métodos

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,26 +1,26 @@
 //import {  dateAscNum,dateDescNum,dateNamAsc,dateNamDesc  } from './dataTest.js';
-import { order1_n, ordern_1, orderA_Z, orderZ_A } from '../src/data.js';
-import {input1_n, output1_n, imputN_1, outputNum_1,imputA_Z, ouputA_Z, imputZ_A, outputZ_A } from './dataTest.js';
+import order from '../src/data.js';
+import { input1_n, output1_n, imputN_1, outputNum_1, imputA_Z, ouputA_Z, imputZ_A, outputZ_A } from './dataTest.js';
 
 
 // describe order ascendente por numero.
-describe("orderldajsflk", () => {
-  it("is a function", () => {
-    expect(typeof order1_n).toBe("function");
-  });
-  it("show ascending  numerical order'num'", () => {
-    expect(order1_n(input1_n)).toEqual(output1_n);
-  });
+describe("order", () => {
+    it("is a function", () => {
+        expect(typeof order.num1_N).toBe("function");
+    });
+    it("show ascending  numerical order'num'", () => {
+        expect(order1_n(input1_n)).toEqual(output1_n);
+    });
 
 
-  // describe order descendente por numero.
-  it("ordern_1", () => {
-    expect(typeof ordern_1).toBe("function");
-  });
+    // describe order descendente por numero.
+    it("ordern_1", () => {
+        expect(typeof order.numN_1).toBe("function");
+    });
 
-  it("show descending  numerical order'num'", () => {
-    expect(ordern_1(imputN_1)).toEqual(outputNum_1);
-  });
+    it("show descending  numerical order'num'", () => {
+        expect(ordern_1(imputN_1)).toEqual(outputNum_1);
+    });
 });
 
 
@@ -28,25 +28,17 @@ describe("orderldajsflk", () => {
 
 // order alfabeticamente
 describe("order", () => {
-  it("is a function", () => {
-    expect(typeof orderA_Z).toBe("function");
-  });
-  it("show ascending  name order 'a-z' ", () => {
-    expect(orderA_Z(imputA_Z)).toEqual(ouputA_Z);
-  });
+    it("is a function", () => {
+        expect(typeof order.nameA_Z).toBe("function");
+    });
+    it("show ascending  name order 'a-z' ", () => {
+        expect(orderA_Z(imputA_Z)).toEqual(ouputA_Z);
+    });
 
-  it("is a function order name descendiente", () => {
-    expect(typeof orderZ_A).toBe("function");
-  });
-  it("show ascending  name order 'a-z' ", () => {
-    expect(orderZ_A(imputZ_A)).toEqual(outputZ_A);
-  });
+    it("is a function order name descendiente", () => {
+        expect(typeof order.nameZ_A).toBe("function");
+    });
+    it("show ascending  name order 'a-z' ", () => {
+        expect(orderZ_A(imputZ_A)).toEqual(outputZ_A);
+    });
 });
-
-
-
-
-
-
-
-
