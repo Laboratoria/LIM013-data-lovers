f97fc6f22d37a6645d2e1970ceb9e3677add48fc
Daniela-delacruz <a.danieladelacruz@gmail.com>
2020-08-19 22:55:38 -0500

    ...

--- src/main.js
+++ src/main.js
@@ -4,15 +4,6 @@ import data from './data/lol/lol.js';
 /*console.log(example, data);*/
 
 
-//Creamos funcion para crear cards
-function cardsLegends(id, name, tittle) {
-    const section=document.createElement("div");
-    const textImagen=document.createElement(imagen);
-    const infoChampiom=document.createElement("div");
-
-    infoChampiom.innerHTML += "<p> id + name + tittle </p>";
-}
-
 
 
 
