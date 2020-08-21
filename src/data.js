283e921e1469af13edbc62c4edb06977b4b10dc3
Julieth-Salcedo <Julieth-Salcedo>
2020-08-21 13:44:33 -0500

    filter, sort

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterByType = (array,type) => array.filter (x => x.type.includes(type))
+;
+
+export const sortByName = (array,condition) => {
+  let newArray = array.sort((a,b) => a.name>b.name ? 1:-1);
+  if ( condition === "2" ) {array.sort((a,b) => a.name<b.name ? 1:-1);}
+  if ( condition === "3" ) {array.sort((a,b) => a.num-b.num);} 
+  if ( condition === "4" ) {array.sort((a,b) => b.num-a.num);}
 };
 
-export const anotherExample = () => {
+export const findByName = () => {
   return 'OMG';
 };
+
