df7861fc138a7feb309860d6bf9493f6355c352f
Diana Sánchez Ordóñez <dianaordonez1998@gmail.com>
2020-08-22 20:12:00 -0500

    conflicto_02

--- src/data.js
+++ src/data.js
@@ -1,12 +1,3 @@
-const filterLegend = (data, rol) => {
-  const result = data.filter(
-    (objLegend) => {
-      return objLegend.tags.includes(rol);
-    }
-  )
-  return result
-}
 
-export default filterLegend;
 
  
