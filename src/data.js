3056a375c51efb2e825ab021d61703d350565f13
milusca_judith <miluscavega15@gmail.com>
2020-08-23 15:43:58 -0500

    modifique css de la segunda pagina y abri otro archivo js para los slides

--- src/data.js
+++ src/data.js
@@ -21,24 +21,3 @@ const getTopFive = (data) => {
   return newArrayOrder.slice(0, 5);
 };
 
-
-function containerFunctions(array) {
-  // const array = Object.values(LOL.data);
-  // Segundo diseño de templates
-  const arrayTopFive = lol.getTopFive(array);
-  // templatepara el top ten
-  const templateListChampionTop = (list) => {
-    let championsList = [];
-    list.forEach((array) => {
-      const templateList =
-        `<div class='card-1'>
-      <img class='champion' src='lol.js/${array.name}.png'></img>
-      <h2 class='namechampion'>"${array.name}"</h2>
-      <h3 class='hptext'><img class='img-hp' src='imagenes/puntovida.PNG'>PUNTOS DE VIDA : ${array.hp}</h3>
-      </div>`;
-      championsList += templateList;
-    });
-    document.getElementById('list-top-champions').innerHTML = championsList;
-  };
-  templateListChampionTop(arrayTopFive);
-}
