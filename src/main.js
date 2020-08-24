e067479cad1cca32eed163609e664dc21106b520
Elizabeth Rivera <elizabethriver@gmail.com>
2020-08-24 15:14:49 -0500

    incluir imagenes y agregar html

--- src/main.js
+++ src/main.js
@@ -5,34 +5,33 @@
 
 //console.log(example, data);
 //DOM de slides//
-const slideprev = document.getElementById('prev');
-slideprev.addEventListener('click', () => {
-    plusSlides(-1);
-    });
-const slidenext = document.getElementById('next');
-slidenext.addEventListener('click', () => {
-    plusSlides(1);
-    });
-const slide1 = document.getElementById('dot1');
-slide1.addEventListener("click", ()=>{
-    currentSlide(1);
+const slideprev = document.getElementById("prev");
+slideprev.addEventListener("click", () => {
+  plusSlides(-1);
+});
+const slidenext = document.getElementById("next");
+slidenext.addEventListener("click", () => {
+  plusSlides(1);
+});
+const slide1 = document.getElementById("dot1");
+slide1.addEventListener("click", () => {
+  currentSlide(1);
 });
 
-const slide2 = document.getElementById('dot2');
-slide2.addEventListener("click", ()=>{
-    currentSlide(2);
+const slide2 = document.getElementById("dot2");
+slide2.addEventListener("click", () => {
+  currentSlide(2);
 });
 
-const slide3 = document.getElementById('dot3');
-slide3.addEventListener("click", ()=>{
-    currentSlide(3);
+const slide3 = document.getElementById("dot3");
+slide3.addEventListener("click", () => {
+  currentSlide(3);
 });
 
-const slide4 = document.getElementById('dot4');
-slide4.addEventListener("click", ()=>{
-    currentSlide(4);
+const slide4 = document.getElementById("dot4");
+slide4.addEventListener("click", () => {
+  currentSlide(4);
 });
-
 
 //slides de modo de juego//
 let slideIndex = 1;
@@ -40,34 +39,31 @@ showSlides(slideIndex);
 let num = [];
 
 function plusSlides(num) {
-  showSlides(slideIndex += num);
+  showSlides((slideIndex += num));
 }
 plusSlides(num);
 
 function currentSlide(num) {
-    showSlides(slideIndex = num); 
+  showSlides((slideIndex = num));
 }
 currentSlide(num);
 
 function showSlides(num) {
   let i;
-  const slides = document.getElementsByClassName('mySlides');
-  const dots = document.getElementsByClassName('dot');
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
   if (num > slides.length) {
     slideIndex = 1;
-  }    
+  }
   if (num < 1) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace('active-slide-img', '');
+    dots[i].className = dots[i].className.replace("active-slide-img", "");
   }
-  slides[slideIndex - 1].style.display = 'block';  
-  dots[slideIndex - 1].className += 'active-slide-img';
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += "active-slide-img";
 }
-
-
-
