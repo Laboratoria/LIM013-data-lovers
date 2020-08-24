49a0124d3f8ff3e8679ffd89d49b74d0c3a0faf6
Liliana-Ram <lilirame@gmail.com>
2020-08-24 16:11:29 -0500

    Función de modal

--- src/main.js
+++ src/main.js
@@ -241,6 +241,31 @@ const closeModal = () => {
 const closeFilter = document.getElementById("close");
 closeFilter.addEventListener("click", closeModal);
 
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+	accordionItemHeader.addEventListener("click", event => {
+
+
+
+		const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+		if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+			currentlyActiveAccordionItemHeader.classList.toggle("active");
+			currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+		}
+
+		accordionItemHeader.classList.toggle("active");
+		const accordionItemBody = accordionItemHeader.nextElementSibling;
+		if (accordionItemHeader.classList.contains("active")) {
+			accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+		}
+		else {
+			accordionItemBody.style.maxHeight = 0;
+		}
+
+	});
+});
+
 
 
 // import data from './data/lol/lol.js';
