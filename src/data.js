d4e943ce543c3e9fd0df9dcb9fb1ac23e6bebc4a
Vero Bc <verobc.cel@gmail.com>
2020-08-24 22:19:12 -0500

    filter and order functions

--- src/data.js
+++ src/data.js
@@ -1,210 +1,177 @@
-export const pullData = (url) => { 
-    let promise = fetch(url)                          // Call the fetch function passing the url of the API as a parameter
-                    .then(data => data.json())        // Transform the data into json
-                    .catch(function() {               // This is where you run code if the server returns any errors
-                    console.error("Algo falló");
-                    });        
-    return promise;
-};
 
-export const dataApi = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-   
-export const clickButtonPrevious = () => {
-    let url = document.getElementById('previous').getAttribute('url');
-    dataApi(url);
-};
+export const filterSpecies = {
 
-export const clickButtonNext = () => {
-    let url = document.getElementById('next').getAttribute('url');
-    dataApi(url);
-};
-
-// Creating the list
-// Adding li for each character, adding a text for each li
-// We are using the prefix "element" for the DOM elements (document object model)
-
-export const getList = (data) => {                             //This data is the Received Info
-
-    const elementOrderedList = document.getElementById('listCharacters');
-    elementOrderedList.innerHTML = "";
-
-    for (let i=0; i < data.results.length; i++) {       // Takes "i-ésim0" results so 20 (it means, one character)
+    human: (allCharacters) => {
         
-        const result = data.results[i];                 
-        const name = result.name;                               // Takes the name of the character
-        const image = result.image;                             // Takes the image of the character
-        const elementLi = document.createElement("li");         // Create a <li> node
-        const elementText = document.createTextNode(name + ", Specie: " + result.species + ", Gender: " + result.gender);         // Create a text node
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Human";
+        });
+    },
+
+    alien: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Alien";
+
+        });
+    },    
+
+    humanoid: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Humanoid";
+
+        });
+    },
+
+    animal: (allCharacters) => {
         
-        elementLi.appendChild(elementText); 
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Animal";
+        });
+    },
 
-        const elementImage = document.createElement("img");     // Create a <li> node
-        elementImage.src = image;
-        elementImage.alt = name;
-        elementLi.appendChild(elementImage); 
+    mytholog: (allCharacters) => {
 
-        elementOrderedList.appendChild(elementLi);
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Mytholog";
 
-    }
+        });
+    },    
 
-    const elementButtonPrevious = document.getElementById('previous');
-    elementButtonPrevious.setAttribute("url", data.info.prev);
+    robot: (allCharacters) => {
 
-    const elementButtonNext = document.getElementById('next');
-    elementButtonNext.setAttribute("url", data.info.next);
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Robot";
 
-}
+        });
+    },
+
+    unknown: (allCharacters) => {
         
-dataApi();
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "unknown";
+        });
+    },
 
+    cronenberg: (allCharacters) => {
 
-// We start to filter by species using filter APIs links
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Cronenberg";
 
-export const dataApiHuman = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=human";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
+        });
+    },    
+
+    poopybutthole: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Poopybutthole";
+
+        });
+    },
+
+    disease: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.species == "Disease";
+
+        });
+    },    
 };
 
-export const clickHuman = () => {
-    let url = document.getElementById('human').getAttribute('url');
-    dataApiHuman(url);
+
+
+export const filterStatus = {
+
+    alive: (allCharacters) => {
+        
+        return allCharacters.filter(function(filtro) {
+            return filtro.status == "Alive";
+        });
+    },
+
+    dead: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.status == "Dead";
+
+        });
+    },    
+
+    unknown: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.status == "unknown";
+
+        });
+    },    
 };
 
-export const dataApiAlien = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=alien";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
+
+export const filterGender = {
+
+    male: (allCharacters) => {
+        
+        return allCharacters.filter(function(filtro) {
+            return filtro.gender == "Male";
+        });
+    },
+
+    female: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.gender == "Female";
+
+        });
+    },    
+
+    genderless: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.gender == "Genderless";
+
+        });
+    },
+    
+    unknown: (allCharacters) => {
+
+        return allCharacters.filter(function(filtro) {
+            return filtro.gender == "unknown";
+
+        });
+    },
 };
 
-export const clickAlien = () => {
-    let url = document.getElementById('alien').getAttribute('url');
-    dataApiAlien(url);
-};
 
-export const dataApiHumanoid = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=humanoid";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
 
-export const clickHumanoid = () => {
-    let url = document.getElementById('humanoid').getAttribute('url');
-    dataApiHumanoid(url);
-};
+export const order = {
 
-export const dataApiAnimal = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=animal";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
+    ascending: (allCharacters) => {
+        
+        return allCharacters.sort(function(a,b) { 
+    
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (a.name < b.name) {
+                return -1;
+            }
+                return 0;
+        });
+    },
 
-export const clickAnimal = () => {
-    let url = document.getElementById('animal').getAttribute('url');
-    dataApiAnimal(url);
-};
+    descending: (allCharacters) => {
+        
+        return allCharacters.sort(function(a,b) { 
+    
+            if (a.name > b.name) {
+                return -1;
+            }
+            if (a.name < b.name) {
+                return 1;
+            }
+                return 0;
+        });
+    },    
 
-export const dataApiMytholog = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=mytholog";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickMytholog = () => {
-    let url = document.getElementById('mytholog').getAttribute('url');
-    dataApiMytholog(url);
-};
-
-export const dataApiRobot = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=robot";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickRobot = () => {
-    let url = document.getElementById('robot').getAttribute('url');
-    dataApiRobot(url);
-};
-
-export const dataApiUnknown = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=unknown";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickUnknown = () => {
-    let url = document.getElementById('unknown').getAttribute('url');
-    dataApiUnknown(url);
-};
-
-export const dataApiCronenberg = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=cronenberg";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickCronenberg = () => {
-    let url = document.getElementById('cronenberg').getAttribute('url');
-    dataApiCronenberg(url);
-};
-
-export const dataApiPoopybutthole = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=poopybutthole";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickPoopybutthole = () => {
-    let url = document.getElementById('poopybutthole').getAttribute('url');
-    dataApiPoopybutthole(url);
-};
-
-export const dataApiDisease = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=disease";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickDisease = () => {
-    let url = document.getElementById('disease').getAttribute('url');
-    dataApiDisease(url);
-};
-
-export const dataApiPlanet = (url) => {
-    if (!url) 
-        url = "https://rickandmortyapi.com/api/character/?species=planet";
-    pullData(url).then(data => {
-        getList(data);
-    }) 
-};
-
-export const clickPlanet = () => {
-    let url = document.getElementById('disease').getAttribute('url');
-    dataApiPlanet(url);
 };
