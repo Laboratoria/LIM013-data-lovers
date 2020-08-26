8272ec6f2657f1cdcae885c4eff335a60437bf12
VeroFlores <veritocalla@gmail.com>
2020-08-26 17:43:58 -0500

    Se desarrolla la página campeones y realiza la primera función de filtrado

--- src/data.js
+++ src/data.js
@@ -1,15 +1,24 @@
 // estas funciones son de ejemplo
-import lol from './data/lol/lol.js'
-console.log(lol);
-const objectLol=lol.data;
-console.log(Object.values(objectLol));
-
-
-export const example = () => {
-  return 'example';
+/*import data from './data/lol/lol.js'
+const objectLol=data.data*/
+//export const getNewArray=championList=>Object.values(championList);//
+export const filterByRol= (championList,term) => {
+    const filterRol= championList.filter(champion => (champion.tags.indexOf(term) !== -1 ? champion : false));
+    console.log(filterRol)
+    return filterRol;
 };
+export const  filtrarCamp=(championList,term)=>{
+const filterData=championList.filter(el=>el.tags[0]==term|| el.tags[1]==term) ;
+console.log(filterData)
+    return filterData;
+}
+/*
+export const search=(championList,nameChamp)=>{
+    const searchName=championList.map(el=>(el.name.toLowerCase().indexOf(nameChamp)!==-1?el:false))
+    console.log(searchName)
+    return search; 
+}*/
+    
 
+ 
 
-export const anotherExample = () => {
-  return 'OMG';
-};
