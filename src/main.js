4e518afc1fcb8e26fc87ac7da20e5198863e7b9b
SheylaRamirezV <68232942+SheylaRamirezV@users.noreply.github.com>
2020-08-30 23:41:54 -0500

    Imprimir imagenes de los pokemones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data);
+
+const pokemonData= data.pokemon;
+const divData= document.getElementById("dataCompleta");
+
+divData.innerHTML= `
+<h1 class="app-title"> Tenemos (${pokemonData.length}) pokemones </h1>
+${pokemonData.map(function(poke){
+    return`
+    <div class="dataPoke">
+        <img src="${poke.img}">
+    </div>
+    `
+}).join("")}
+`
+
+
+  
