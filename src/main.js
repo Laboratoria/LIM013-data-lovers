27b55244da54c8a45cc8f8c75d191234e29cf62c
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-08-31 22:10:28 -0500

    insertando etiquetas al DOM para dar estilo a la dataPokemon

--- src/main.js
+++ src/main.js
@@ -7,18 +7,46 @@ console.log(example, data); */
 
 import data from './data/pokemon/pokemon.js';
 
-for (let i=0;i<251;i++) {
-	/* CREANDO VARIABLES */
-let num=data.pokemon[i].num;
-let name=data.pokemon[i].name;
-let img=data.pokemon[i].img;
-	/* CREANDO ELEMENTOS */	
-let labelImg=document.createElement("img");
-let labelP=document.createElement("p");
-	/* VISUALIZANDO EN EL NAVEGADOR */
-labelImg.setAttribute("src",img);
-labelP.innerHTML= num+" "+name;
-document.getElementById ("dataPoke").appendChild(labelImg);
-document.getElementById ("dataPoke").appendChild(labelP);
+for (let i = 0; i < data.pokemon.length; i++) {
+	// CREANDO VARIABLES
+	let num = data.pokemon[i].num;
+	let name = data.pokemon[i].name;
+	let imgUrl = data.pokemon[i].img;
+	let dataNueva = document.getElementById('dataPoke');
+
+	//AGREGANDO DIV POKEMON
+	let labelPokemon = document.createElement('div');
+	labelPokemon.classList.add('pokemon');
+	dataNueva.appendChild(labelPokemon);
+
+	//AGREGANDO DIV IMG-CONTAINER
+	let labelimgContainer = document.createElement('div');
+	labelimgContainer.classList.add('img-container');
+	labelPokemon.appendChild(labelimgContainer);
+
+	// AGREGANDO IMAGEN
+	let labelImg = document.createElement('img');
+	labelImg.setAttribute('src', imgUrl); // set es asignar , get es obtener (agregando imagen)
+	labelimgContainer.appendChild(labelImg);
+
+	// AGREGANDO DIV INFO
+
+	let labelinfo = document.createElement('div');
+	labelinfo.classList.add('info');
+	labelPokemon.appendChild(labelinfo);
+
+
+	// AGREGANDO NUMERO
+	let labelnumber = document.createElement('span');
+	labelnumber.classList.add('number');
+	labelinfo.appendChild(labelnumber);
+	labelnumber.innerHTML=num;//agregando data de numero
+
+	// AGREGANDO NOMBRE
+	let labelname = document.createElement('h4');
+	labelname.classList.add('name');
+	labelinfo.appendChild(labelname);
+	labelname.innerHTML=name; //agregando data de nombre 
 }
+
 console.log(name);
