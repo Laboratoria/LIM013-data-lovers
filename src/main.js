e014472df08aba6f421eb386d9cbc6ebace1c53b
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-09-02 17:49:55 -0500

    agregando objeto en js para color de fondo por tipo de pokemon

--- src/main.js
+++ src/main.js
@@ -57,5 +57,21 @@ for (let i = 0; i < data.pokemon.length; i++) {
 	labelTipo.innerHTML= tipo;
 }
 
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
 
 console.log(name);
