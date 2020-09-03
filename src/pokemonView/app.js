18dd56e32e8066b248ea1cc42fadde2a0ddace35
vivianBritez <vivianbritez91@gmail.com>
2020-09-03 12:21:32 -0500

    Fetch

--- src/pokemonView/app.js
+++ src/pokemonView/app.js
@@ -1,1 +1,10 @@
 document.getElementById('name').innerHTML = localStorage.getItem('namePokemon');
+// VISUALIZAR CON ARCHIVO .JSON
+
+let pokemones=[];
+    fetch("../data/pokemon/pokemon.json")
+     .then(data => data.json())
+     .then(data => {
+       pokemones = data;
+       console.log(pokemones);
+     });
