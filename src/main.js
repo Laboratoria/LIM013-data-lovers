414bb611dc9a66dddc5c5656ff2cd6d05ca48b41
Shirley Ramos_M <srm.shirley@gmail.com>
2020-09-03 18:44:37 -0500

    changes

--- src/main.js
+++ src/main.js
@@ -2,27 +2,3 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-const importData = data.pokemon;
-
-console.log(example, data);
-
----------------------------------------------
-import data from "./harry-potter.js";
-
-const showListOfCharacters = (charactersArr) => {
-  const containerElement = document.getElementById("app");
-  let listOfCharactersTemplate = "";
-
-  charactersArr.forEach((characterObj) => {
-    const liElement = `
-      <li>
-        ${characterObj.name}
-      </li>
-    `;
-    listOfCharactersTemplate = listOfCharactersTemplate + liElement;
-  });
-
-  containerElement.innerHTML = listOfCharactersTemplate;
-};
-
-showListOfCharacters(data);
