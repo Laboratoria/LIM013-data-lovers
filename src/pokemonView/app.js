ba467538b4eef544e5df713bb1324fb0ce45d0fc
Sharon Huaman <sharonb.huaman@gmail.com>
2020-09-04 11:28:06 -0500

    Basic Data

--- src/pokemonView/app.js
+++ src/pokemonView/app.js
@@ -1,10 +1,29 @@
-document.getElementById('name').innerHTML = localStorage.getItem('namePokemon');
+const namePokemon = localStorage.getItem('namePokemon');
+
+let imgPokemon = document.createElement("img");
+document.getElementById("img-pokemon").appendChild(imgPokemon);
+
 // VISUALIZAR CON ARCHIVO .JSON
 
-let pokemones=[];
-    fetch("../data/pokemon/pokemon.json")
-     .then(data => data.json())
-     .then(data => {
-       pokemones = data;
-       console.log(pokemones);
-     });
+let pokemones = [];
+fetch("../data/pokemon/pokemon.json")
+    .then(data => data.json())
+    .then(data => {
+        pokemones = data.pokemon;
+        let pokemon = pokemones.filter((pokemon) => pokemon.name.includes(namePokemon));
+        basicData(pokemon);
+    });
+
+let basicData = (pokemon) => {
+    const num = pokemon[0].num;
+    imgPokemon.setAttribute("src", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + num + ".png");
+    imgPokemon.setAttribute("class", "img-pokemon-img")
+    document.getElementById("num").innerHTML = "# " + pokemon[0].num;
+    document.getElementById("name").innerHTML = pokemon[0].name;
+    document.getElementById("about").innerHTML = pokemon[0].about;
+    document.getElementById("container").classList.add(pokemon[0].type[0]);
+}
+
+let table = (pokemon) => {
+
+}
