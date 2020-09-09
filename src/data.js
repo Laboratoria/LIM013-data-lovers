2105ee8240cadf935a59f18ef9e5a96291cda696
estefaniamoncada <estefaniamr716@gmail.com>
2020-09-09 10:50:46 -0500

    actualizando commit

--- src/data.js
+++ src/data.js
@@ -19,69 +19,16 @@ export const orderData = (data, condition) => {
   return result;
 };
 //FIN DE FUNCION ORDENAR DATA
-
-//FUNCION FILTRAR
 export const filterDataType = (arrayFilter, typePokemon) => {
-<<<<<<< HEAD
-=======
 	if (typePokemon !== '' && typePokemon !== 'all') {
 		arrayFilter = arrayFilter.filter(
 			(pokemon) => pokemon.type.indexOf(typePokemon) !== -1
 		);
 	}
->>>>>>> upstream/master
 
-  if (typePokemon !== '' && typePokemon !== 'all') {
-    arrayFilter = arrayFilter.filter(
-      (pokemon) => pokemon.type.indexOf(typePokemon) !== -1
-    );
-  }
+	return arrayFilter;
+};
 
-  export const orderData = (data, condition) => {
-    let result;
-    if (condition === 'a-z') {
-      result = data.sort((a, b) => {
-        /* return (a.name>b.name ? 1:  -1) */
-
-        if (a.name > b.name) {
-          return 1;
-        }
-        return -1;
-      });
-    }
-    else {
-      result = data.sort((a, b) => {
-        /*  return (a.name<b.name ? 1:  -1) */
-        if (a.name < b.name) {
-          return 1;
-        }
-        return -1;
-      });
-
-<<<<<<< HEAD
-    }
-    return result;
-  }
-}
-
-
-// export const filterData = (data, condition) => {
-//   const conditionType = condition[0].value;
-//   const conditionEgg = condition[1].value;
-//   const conditionRegion = condition[2].value;
-//   let arrFilter = data;
-//   if (conditionType !== '' && conditionType !== 'all') {
-//     arrFilter = arrFilter.filter((infoPokemon) => infoPokemon.type.indexOf(conditionType) !== -1);
-//   }
-//   if (conditionEgg !== '' && conditionEgg !== 'all') {
-//     arrFilter = arrFilter.filter((infoPokemon) => infoPokemon.egg === conditionEgg);
-//   }
-//   if (conditionRegion !== '' && conditionRegion !== 'all') {
-//     arrFilter = arrFilter.filter((infoPokemon) => infoPokemon.generation.name === conditionRegion);
-//   }
-//   return arrFilter;
-// }; 
-=======
 export const filterDataGeneracion = (arrayFilter, genepokemon) => {
 	if (genepokemon !== '' && genepokemon !== 'all') {
 		arrayFilter = arrayFilter.filter(
@@ -102,4 +49,3 @@ export const filterDataEgg = (arrayFilter, eggpokemon) => {
 
 
 //FIN DE FUNCION FILTRAR
->>>>>>> upstream/master
