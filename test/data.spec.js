5df777ac70af04c04418217082331e46f788b457
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-09-08 19:40:27 -0500

    Agregando mas funciones para test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,50 +1,25 @@
 import {filterForNumber, extractImgPok,sortByAsc, sortByDesc } from '../src/data.js';
 import {sortByLessPwr, sortByMorePwr,filterByType, searchByNameandNum } from '../src/data.js';
 
+const dataPok = [{"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"],"stats":{"max-hp":"10"}},
+                {"num": "002","name": "ivysaur","img": "002.png","type":["grass", "poison"],"stats":{"max-hp":"30"}},
+                {"num": "003","name": "venusaur","img": "003.png","type":["rock"],"stats":{"max-hp":"40"}},
+                {"num": "004","name": "charmander","img": "004.png","type":["fire"],"stats":{"max-hp":"20"}}];
 
 
-
-/*describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+const arrayPok003 =[{"num": "003","name": "venusaur","img": "003.png","type":["rock"],"stats":{"max-hp":"40"}}];
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});*/
-const dataPok = [{"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"]},
-                {"num": "002","name": "ivysaur","img": "002.png","type":["grass", "poison"]},
-                {"num": "003","name": "venusaur","img": "003.png","type":["rock"]},
-                {"num": "004","name": "charmander","img": "004.png","type":["fire"]}];
-
-const arrayPok003 =[{"num": "003","name": "venusaur","img": "003.png","type":["rock"]}];
-
-const arrayPok001002=[{"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"]},
-                      {"num": "002","name": "ivysaur","img": "002.png","type":["grass", "poison"]}];
-
-
-const resultsortByAsc=[{"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"]},
-                      {"num": "004","name": "charmander","img": "004.png","type":["fire"]},
-                      {"num": "002","name": "ivysaur","img": "002.png","type":["grass", "poison"]},
-                      {"num": "003","name": "venusaur","img": "003.png","type":["rock"]}];
+const resultsortByAscLP=[{"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"],"stats":{"max-hp":"10"}},
+                        {"num": "004","name": "charmander","img": "004.png","type":["fire"],"stats":{"max-hp":"20"}},
+                        {"num": "002","name": "ivysaur","img": "002.png","type":["grass","poison"],"stats":{"max-hp":"30"}},
+                        {"num": "003","name": "venusaur","img": "003.png","type":["rock"],"stats":{"max-hp":"40"}}];
                       
-const resultsortByDesc=[{"num": "003","name": "venusaur","img": "003.png","type":["rock"]},
-                      {"num": "002","name": "ivysaur","img": "002.png","type":["grass", "poison"]},
-                      {"num": "004","name": "charmander","img": "004.png","type":["fire"]},
-                      {"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"]}];
-                 
+const resultsortByDescMP=[{"num": "003","name": "venusaur","img": "003.png","type":["rock"],"stats":{"max-hp":"40"}},
+                        {"num": "002","name": "ivysaur","img": "002.png","type":["grass","poison"],"stats":{"max-hp":"30"}},
+                        {"num": "004","name": "charmander","img": "004.png","type":["fire"],"stats":{"max-hp":"20"}},
+                        {"num": "001","name": "bulbasaur","img": "001.png","type":["grass", "poison"],"stats":{"max-hp":"10"}}];
+                              
 describe('filtrar por número de pokedex', () => {
   it('is a function', () => {
     expect(typeof filterForNumber).toBe('function');
@@ -73,7 +48,10 @@ describe('Devolver array ordenado de forma ascendente', () => {
     expect(typeof sortByAsc).toBe('function');
   });
   it('return dataPok ordenado de A-Z', () => {
-    expect(sortByAsc(dataPok)).toEqual(resultsortByAsc);
+    expect(sortByAsc(dataPok)).toEqual(resultsortByAscLP);
+  });
+  it('return array de longitud 4', () => {
+    expect(sortByAsc(dataPok)).toHaveLength(4);
   });
   
 });
@@ -83,7 +61,10 @@ describe('Devolver array ordenado de forma descendente', () => {
     expect(typeof sortByDesc).toBe('function');
   });
   it('return dataPok ordenado de Z-A', () => {
-    expect(sortByDesc(dataPok)).toEqual(resultsortByDesc);
+    expect(sortByDesc(dataPok)).toEqual(resultsortByDescMP);
+  });
+  it('return array de longitud 4', () => {
+    expect(sortByDesc(dataPok)).toHaveLength(4);
   });
   
 });
@@ -92,13 +73,26 @@ describe('Devolver array ordenado de +poder a -poder', () => {
   it('is a function', () => {
     expect(typeof sortByLessPwr).toBe('function');
   });
-  
+  it('return dataPok ordenado de menor a mayor poder', () => {
+    expect(sortByLessPwr(dataPok)).toEqual(resultsortByDescMP);
+  });
+  it('return array de longitud 4', () => {
+    expect(sortByLessPwr(dataPok)).toHaveLength(4);
+  });
+
 });
 
 describe('Devolver array ordenado de -poder a +poder', () => {
   it('is a function', () => {
     expect(typeof sortByMorePwr).toBe('function');
   });
+  it('return dataPok ordenado de mayor a menor poder', () => {
+    expect(sortByMorePwr(dataPok)).toEqual(resultsortByDescMP);
+  });
+  it('return array de longitud 4', () => {
+    expect(sortByMorePwr(dataPok)).toHaveLength(4);
+  });
+  
   
 });
 
@@ -109,8 +103,8 @@ describe('Devolver array cuyo tipo sea el mismo que introducimos', () => {
   it('return array con un objeto cuyo tipo ="rock"', () => {
     expect(filterByType(["rock"],dataPok)).toEqual(arrayPok003);
   });
-  it('return array con un objeto cuyo tipo ="rock"', () => {
-    expect(filterByType(["grass", "poison"],dataPok)).toEqual(arrayPok001002);
+  it('return array con dos elementos', () => {
+    expect(filterByType(["grass","poison"] , dataPok)).toHaveLength(2);
   });
   
 });
@@ -125,6 +119,9 @@ describe('Devolver un array que cumpla con a condición de search', () => {
   });
   it('return array con un objeto cuyo name=venusaur', () => {
     expect(searchByNameandNum("venusaur",dataPok)).toEqual(arrayPok003);
+  });
+  it('return array vacio', () => {
+    expect(searchByNameandNum("prueba",dataPok)).toEqual([]);
   });
   
 });
